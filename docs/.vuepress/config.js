8575c5e93c6a8a1bf7c4b45df8d1a82f91fc85a7
thony <a0982830615@gmail.com>
2019-09-27 15:53:10 +0800

    Add

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'MyGitBook',
+    description: 'MyGitBook',
+    base: '/mygitbook/',
+    repo: 'https://github.com/thonyl19/mygitbook',
+    themeConfig: {
+      sidebarDepth: 0,
+      sidebar: [
+        ['/', '前言']
+      ],
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Github', link: '/' }
+      ]
+    }
+ 
+  }
