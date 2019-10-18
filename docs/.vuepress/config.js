195d6b58154d86fe022ddb9f60e1333b9eba8d24
thony <a0982830615@gmail.com>
2019-10-18 14:20:12 +0800

    Adj>the site cover and else setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 let themeConfig_ououe = {
     showThemeButton: false,
-    // cover: '/cover.jpg',
-    // logo: '/logo.png',
+    cover: 'https://picsum.photos/id/830/1920/1080',
+    //logo: 'https://picsum.photos/1920/1080/?random&date=2019-02-22',
     search: true,
     backgroundImage: false,
     pageGroup: 5,
@@ -18,9 +18,22 @@ let themeConfig_ououe = {
       { text: 'About', link: '/about/' }
     ],
     footer: [
-      { text: 'Github', link: 'https://github.com/tolking' }
+      { text: 'Github', link: 'https://github.com/thonyl19/gitbook' }
     ]
 }
+
+let themeConfig_ououe_head = [
+  //['link', { rel: 'icon', href: '/favicon.ico' }],
+  ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' }],
+  ['meta', { name: 'renderer', content: 'webkit' }],
+  ['meta', { name: 'force-rendering', content: 'webkit' }],
+  ['meta', { name: 'applicable-device', content: 'pc,mobile' }],
+  ['meta', { name: 'author', content: 'Anthony<a0982830615@gmail.com>' }],
+  ['meta', { name: 'copyright', content: '網站內容版權所有，轉載請註明出處' }],
+  ['meta', { name: "keywords", content: "m-less, less, mixins, 常用 mixins 文件, mixins 总结, mixins 整理, less 总结整理, less 技巧, css 简写, ououe, 雨无声, tolking, 前端技巧, 前端总结, 前端分享, ououe.com" }]
+  
+]
+
 
 let themeConfig_old ={
       sidebarDepth: 0,
@@ -64,9 +77,13 @@ module.exports = {
     },
     title: 'Anthony - GitBook',
     description: 'Anthony GitBook',
-    base: '/gitbook/',
+    //base: '/gitbook/',
+    base: '/',
+    dest: 'dist/',
+    evergreen: true,
     repo: 'https://github.com/thonyl19/gitbook',
     theme: 'ououe',
+    head:themeConfig_ououe_head,
     themeConfig: themeConfig_ououe,
     plugins: ['@vuepress/pwa',{
       serviceWorker: true,
