d3ae9c7655dc78e9df2a9850b50b42cc403a4c92
thony <a0982830615@gmail.com>
2019-10-18 12:33:59 +0800

    Add>use vuepress-theme-ououe

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,62 @@
+let themeConfig_ououe = {
+    showThemeButton: false,
+    // cover: '/cover.jpg',
+    // logo: '/logo.png',
+    search: true,
+    backgroundImage: false,
+    pageGroup: 5,
+    postTime: {
+      createTime: 'Create Time',
+      lastUpdated: 'Last Updated'
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Posts', link: '/posts/' },
+      { text: 'Doc', link: '/doc/' },
+      { text: 'Tags', link: '/tag/' },
+      { text: 'Categories', link: '/category/' },
+      { text: 'About', link: '/about/' }
+    ],
+    footer: [
+      { text: 'Github', link: 'https://github.com/tolking' }
+    ]
+}
+
+let themeConfig_old ={
+      sidebarDepth: 0,
+      head:[],
+      sidebar: [
+        ['/', '前言']
+      ],
+      themeConfig: {
+        lastUpdated: true,
+        serviceWorker: {
+          updatePopup: true // Boolean | Object, 默认值是 undefined.
+          // 如果设置为 true, 默认的文本配置将是: 
+          // updatePopup: { 
+          //    message: "New content is available.", 
+          //    buttonText: "Refresh" 
+          // }
+        },
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Github', link: '/' }
+        ]
+      }
+    }
+
 module.exports = {
+  // '@vuepress/back-to-top': true,
+  //   '@vuepress/medium-zoom': true,
+  //   '@vuepress/pwa': {
+  //            serviceWorker: true,
+  //             popupComponent: 'MySWUpdatePopup',
+  //             updatePopup: {
+  //             message: "啦啦啦,一大波内容已更新!",
+  //             buttonText: "朕知道了"
+  //           }
+  //         },
+
     locales: {
       '/':{
         lang:'zh-cn' 
@@ -8,18 +66,10 @@ module.exports = {
     description: 'Anthony GitBook',
     base: '/gitbook/',
     repo: 'https://github.com/thonyl19/gitbook',
-    themeConfig: {
-      sidebarDepth: 0,
-      head:[],
-      sidebar: [
-        ['/', '前言']
-      ],
-      themeConfig: {
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Github', link: '/' }
-        ]
-      }
-    }
- 
+    theme: 'ououe',
+    themeConfig: themeConfig_ououe,
+    plugins: ['@vuepress/pwa',{
+      serviceWorker: true,
+      updatePopup: true
+    }],
   }
